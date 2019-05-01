10c3704e4872e36782905bb2ab40b556597f1faf
shaadibo <colbertrashaad@gmail.com>
2019-05-01 05:05:05 +0000

    print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,7 @@ def print_last_digit(number):
     remainder = number % 10
     negative = -number % 10
     if number >= 0:
-        print ("{:d}".format(remainder), end="")
+        print("{:d}".format(remainder), end="")
     else:
-        print ("{:d}".format(negative), end="")
+        print("{:d}".format(negative), end="")
     return negative
