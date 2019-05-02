bf32407378b3226efd7ec59cfc43159e3b6827e2
shaadibo <colbertrashaad@gmail.com>
2019-05-02 19:17:02 +0000

    infiinte add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print("{}".format(sum(int(l) for l in argv[1:])))
+    print("{:d}".format(sum(int(l) for l in argv[1:])))
 
