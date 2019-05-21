0b5b5086a8315895e2bbd611f640e349b3a77fd2
shaadibo <colbertrashaad@gmail.com>
2019-05-21 03:30:08 +0000

    safe print

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ValueError:
         return None
     finally:
-        print("Inside Result: {}".format(result))
+        print("Inside result: {}".format(result))
         return result
