a24228e23407b95553f956c71b9d238162072992
shaadibo <colbertrashaad@gmail.com>
2019-06-07 06:42:37 +0000

    pascal

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    p = []
+    if n <= 0:
+        return p
+    for t in range(n):
+        if i < 2:
+            p.append([1] * (i + 1))
+        else:
+            return 
