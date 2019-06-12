b6b46c077797a2171ae61a32d59421dc1303e242
shaadibo <colbertrashaad@gmail.com>
2019-06-12 23:33:34 +0000

    base module

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,52 @@ class Base():
             with open(cls.__name__ + ".json", "w+") as f:
                 f.write(Base.to_json_string([x.to_dictionary()
                                             for x in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''returns list of JSON string representation'''
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """dict to instance"""
+        if dictionary is None:
+            return
+        if cls.__name__ == "Base":
+            if "id" in dictionary.keys():
+                return cls(dictionary["id"])
+            else:
+                return cls()
+        else:
+            retcls = cls(4, 4, 4)
+            retcls.update(**dictionary)
+            return retcls
+
+    @classmethod
+    def load_from_file(cls):
+        '''loads from a file'''
+        try:
+            with open(cls.__name__ + ".json", "r") as f:
+                j = cls.from_json_string(f.read())
+            return [cls.create(**a) for a in j]
+        except:
+            return []
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """load from csv file"""
+        with open(cls.__name__ + ".csv") as f:
+            lfg = []
+            lob = []
+            r = csv.DictReader(f)
+            for l in r:
+                lfg.append(l)
+            for a in lfg:
+                for key in l.keys():
+                    l[key] = int(l[key])
+            for c in lfg:
+                lob.append(cls.create(**d))
+            return lob
