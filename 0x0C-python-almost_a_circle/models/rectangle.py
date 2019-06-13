f04ed7f8893d95c604866f07651539dc4ecd1388
shaadibo <colbertrashaad@gmail.com>
2019-06-13 16:07:58 +0000

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -60,13 +60,13 @@ class Rectangle(Base):
         '''y getter'''
         return (self.__y)
 
-    @x.setter
+    @y.setter
     def y(self, value):
         ''' y setter'''
         if not isinstance(value, int):
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("y must be > 0")
         self.__y = value
 
     def area(self):
