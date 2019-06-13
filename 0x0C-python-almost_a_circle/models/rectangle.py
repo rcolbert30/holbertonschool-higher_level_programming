721416d132c799f933d42cd68ffae1b9da753ba3
shaadibo <colbertrashaad@gmail.com>
2019-06-13 00:52:39 +0000

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,10 +23,9 @@ class Rectangle(Base):
         '''width setter'''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -38,10 +37,9 @@ class Rectangle(Base):
         ''' height setter'''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def x(self):
@@ -53,10 +51,9 @@ class Rectangle(Base):
         '''x setter '''
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("x must be > 0")
-        else:
-            self.__x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -68,10 +65,9 @@ class Rectangle(Base):
         ''' y setter'''
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("x must be > 0")
-        else:
-            self.__y = value
+        self.__y = value
 
     def area(self):
         '''area method'''
