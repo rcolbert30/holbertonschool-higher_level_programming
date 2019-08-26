a319e34d05affdf7c5b99c3cdfafd43a206db4e5
rcolbert30 <colbertrashaad@gmail.com>
2019-08-26 21:21:34 +0000

    post email, requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,5 +4,5 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
 
-    r = requests.port(url, data = {'email' : argv[2]})
+    r = requests.port(url, data={'email': argv[2]})
     print(r.text)
