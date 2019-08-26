45ef56fc66eb2a3921d19be61fc1e1e054ecbead
rcolbert30 <colbertrashaad@gmail.com>
2019-08-26 19:29:37 +0000

    request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@ import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-        print('Body resposne:')
+        print('Body response:')
         response = r.read()
         print('\t- type: {}'.format(type(response)))
         print('\t- content: {}'.format(response))
