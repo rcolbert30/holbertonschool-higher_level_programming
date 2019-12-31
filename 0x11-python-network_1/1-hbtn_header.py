5ff19cc80f308b36a70457985e5048bddb872ec7
Rashaad Colbert <colbertrashaad@gmail.com>
2019-12-30 18:31:05 -0800

    Delete 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import urllib.request
-from sys import argv
-
-if __name__ == "__main__":
-    url = argv[1]
-
-    with urllib.request.urlopen(url) as response:
-        print(response.info().get('X-Request-Id'))
