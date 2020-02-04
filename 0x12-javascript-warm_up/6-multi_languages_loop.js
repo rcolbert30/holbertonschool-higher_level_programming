f57d562043255b1b79a026b5159cb344b89e4492
rcolbert30 <colbertrashaad@gmail.com>
2020-02-04 22:18:30 +0000

    loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints from an array using a loop
+
+const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+words.forEach(function (item, array) {
+  console.log(item);
+});
