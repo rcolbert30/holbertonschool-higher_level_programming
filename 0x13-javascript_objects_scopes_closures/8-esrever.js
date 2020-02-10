6813ecd9408b14ac4c0c9d2f63c90817f1c8bcc3
rcolbert30 <colbertrashaad@gmail.com>
2020-02-10 21:06:57 +0000

    eserver

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,5 +2,9 @@
 // return reversed version of a list
 
 exports.esrever = function (list) {
-  return (list.reverse());
+  const newList = [];
+  for (let i = list.length - 2; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return (newList);
 };
