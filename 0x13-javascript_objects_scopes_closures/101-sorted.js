d98eda9c135a2baf4c92d1e55e2ffd667321badb
rcolbert30 <colbertrashaad@gmail.com>
2020-02-11 06:49:36 +0000

    sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/* imports array and computes a new array
-   creates a new list, new value is multipled by its index in the list */
 
-const list = require('./100-data').list;
+const dict = require('./101-data').dict;
 
-console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(Object.entries(dict).reduce(function (x, y) {
+  x[y[1]] = (x[y[1]] || []).concat(y[0]);
+  return x;
+}, {}));
